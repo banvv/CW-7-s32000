4c8d821daa0c6b3a0f5b5cf4e069212df95f6f31
dblvv <codejot@gmail.com>
2025-05-05 15:06:15 +0200

    feat(): add the rest of the dtos at model

---
+++ Models/DTOs/ClientCreateDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SqlClient.Models.DTOs;
+
+public class ClientCreateDTO
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Telephone { get; set; }
+    public string Pesel { get; set; }
+}
---
+++ Models/DTOs/TripGetDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace SqlClient.Models.DTOs;
+
+public class TripGetDTO
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int DateFrom { get; set; }
+    public int DateTo { get; set; }
+    public int MaxPeople { get; set; }
+}
